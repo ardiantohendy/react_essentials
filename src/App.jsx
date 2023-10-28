51bd0b01cfde7c4a454856ad43c31ead05f24984
Ardiantohendy <bayuardianto421@gmail.com>
2023-10-29 01:14:18 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import Header from "./components/Header";
+import { CORE_CONCEPTS } from "./data";
+import CoreConcepts from "./components/CoreConceptsCard";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            {/* hard ways */}
+            <CoreConcepts title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} image={CORE_CONCEPTS[0].image} />
+            {/* easy ways */}
+            <CoreConcepts {...CORE_CONCEPTS[1]} />
+            <CoreConcepts {...CORE_CONCEPTS[2]} />
+            <CoreConcepts {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
