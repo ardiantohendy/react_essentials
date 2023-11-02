f7a2a762f54fd5cb5a76c8b941b6289a5f7c47f6
Ardiantohendy <bayuardianto421@gmail.com>
2023-11-02 15:28:51 +0700

    new update

--- src/App.jsx
+++ src/App.jsx
@@ -19,12 +19,21 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
+            {/* with map method */}
+            {CORE_CONCEPTS.map((conceptItem) => (
+              <CoreConcepts {...conceptItem} />
+            ))}
+
+            {/* manual method */}
             {/* hard ways */}
-            <CoreConcepts title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} image={CORE_CONCEPTS[0].image} />
+
+            {/* <CoreConcepts title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} image={CORE_CONCEPTS[0].image} /> */}
+
             {/* easy ways */}
-            <CoreConcepts {...CORE_CONCEPTS[1]} />
+
+            {/* <CoreConcepts {...CORE_CONCEPTS[1]} />
             <CoreConcepts {...CORE_CONCEPTS[2]} />
-            <CoreConcepts {...CORE_CONCEPTS[3]} />
+            <CoreConcepts {...CORE_CONCEPTS[3]} /> */}
           </ul>
         </section>
         <section id="examples">
